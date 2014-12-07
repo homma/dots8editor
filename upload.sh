274c458afce55beb510ebf3c3964df300d265d28
Daisuke Homma <daisuke.homma@gmail.com>
2014-12-07 18:17:00 +0900

    committed on 2014/12/07 18:16.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#### written by Daisuke Homma
+##
+## first, create repo from github.com, click '+' menu to create new repository.
+## then run below in the project directory.
+## git init
+## git commit -m 'first commit'
+## git remote add origin https://github.com/<user>/<project>.git
+## git push -u origin master
+## having done, then run this script.
+##
+
 DATE=`date '+%Y/%m/%d %H:%M'`
 MESSAGE="committed on ${DATE}."
 
